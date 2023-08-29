a7185ab99398c719e334b6fc108a9ab6e8c354c8
l4mers <gustav@henrikssons.org>
2023-08-29 11:44:34 +0200

    asd

---
+++ it-security2/src/main/java/com/example/itsecurity2/ItSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.example.itsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSecurity2Application.class, args);
+    }
+
+}
---
+++ it-security2/src/test/java/com/example/itsecurity2/ItSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
